69136cca9e529bfc825062c751bf7a129b352b08
Nandha kumar S <mailstonandhakumar@gmail.com>
2017-06-28 14:47:39 +0530

    Create porn.c

---
+++ porn.c
@@ -0,0 +1,14 @@
+void main()
+{
+int a;
+clrscr();
+printf("\nNumber:",a);
+scanf("\n%d",&a);
+if(a>0)
+printf("\npositive");
+else if(a<0)
+printf("\nNegative");
+else
+printf("\nZero");
+getch();
+}
